a814b112b036c8eab182023507ffb75037ea617a
sucre <guoshengw24@gmail.com>
2025-09-22 23:54:54 +0800

    feat: add led drv

---
+++ 03_driver/02_led/led.c
@@ -0,0 +1,135 @@
+#include "drv.h"
+
+#define LED_MAJOR 200
+#define LED_NAME  "led"
+
+#define LED_OFF   0
+#define LED_ON    1
+
+/* register physical address */
+#define CCM_CCGR1_BASE         (0X020C406C)
+#define SW_MUX_GPIO1_IO03_BASE (0X020E0068)
+#define SW_PAD_GPIO1_IO03_BASE (0X020E02F4)
+#define GPIO1_DR_BASE          (0X0209C000)
+#define GPIO1_GDIR_BASE        (0X0209C004)
+
+/* mapped register virtual address pointer */
+static void __iomem *IMX6U_CCM_CCGR1;
+static void __iomem *SW_MUX_GPIO1_IO03;
+static void __iomem *SW_PAD_GPIO1_IO03;
+static void __iomem *GPIO1_DR;
+static void __iomem *GPIO1_GDIR;
+
+void led_switch(u8 sta)
+{
+    u32 val = 0;
+
+    if (sta = LED_ON) {
+        val = readl(GPIO1_DR);
+        val &= ~(1 << 3);
+        writel(val, GPIO1_DR);
+    } else if (sta == LED_OFF) {
+        val = readl(GPIO1_DR);
+        val |= (1 << 3);
+        writel(val, GPIO1_DR);
+    }
+}
+
+static int led_open(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static ssize_t led_read(struct file *filp, char __user *buf, size_t cnt, loff_t *offt)
+{
+    return 0;
+}
+
+static ssize_t led_write(struct file *filp, const char __user *buf, size_t cnt, loff_t *offt)
+{
+    int ret = 0;
+    unsigned char databuf[1] = {0};
+    unsigned char ledstat = 0;
+
+    ret = copy_from_user(databuf, buf, cnt);
+    if (ret < 0) {
+        printk("kernel write failed\n");
+        return -EFAULT;
+    }
+
+    ledstat = datavyf[0];
+
+    if (ledstat == LED_ON) {
+        led_switch(LED_ON);
+    } else if (led_stat == LED_OFF) {
+        led_switch(LED_OFF);
+    }
+}
+
+static int led_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static struct file_operations led_fops = {
+    .owner = THIS_MODULE,
+    .open = led_open,
+    .release = led_release,
+    .read = led_read,
+    .write = led_write,
+};
+
+static int __init led_init(void)
+{
+    int ret = 0;
+    u32 val = 0;
+
+    IMX6U_CCM_CCGR1 = ioremap(CCM_CCGR1_BASE, 4);
+    SW_MUX_GPIO1_IO03 = ioremap(SW_MUX_GPIO1_IO03_BASE, 4);
+    SW_PAD_GPIO1_IO03 = ioremap(SW_PAD_GPIO1_IO03_BASE, 4);
+    GPIO1_DR = ioremap(GPIO1_GDIR_BASE, 4);
+    GPIO1_GDIR = ioremap(GPIO1_GDIR_BASE, 4);
+
+    /* enable clock */
+    val = readl(IMX6U_CCM_CCGR1);
+    val &= ~(3 << 26);
+    val |= (3 << 26);
+    writel(val, IMX6U_CCM_CCGR1);
+    writel(5, SW_MUX_GPIO1_IO03);
+    writel(0x10B0, SW_PAD_GPIO1_IO03);
+
+    val = readl(GPIO1_GDIR);
+    val &= ~(1 << 3);
+    val |= (1 << 3);
+    writel(val, GPIO1_GDIR);
+
+    val = readl(GPIO1_DR);
+    val |= (1 << 3);
+    writel(val, GPIO1_DR);
+
+    ret = register_chrdev(LED_MAJOR, LED_NAME, &led_fops);
+    if (ret < 0) {
+        printk(KERN_INFO, "register chrdev failed\n");
+        return -EIO;
+    }
+
+    return 0;
+}
+
+static void __exit led_exit(void)
+{
+    iounmap(IMX6U_CCM_CCGR1);
+    iounmap(SW_MUX_GPIO1_IO03);
+    iounmap(SW_PAD_GPIO1_IO03);
+    iounmap(GPIO1_DR);
+    iounmap(GPIO1_GDIR);
+
+    unregister_chrdev(led_MAJOR, LED_NAME);
+    
+    printk("remod led");
+}
+
+module_init(led_init);
+module_exit(led_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("xuemanyi");
